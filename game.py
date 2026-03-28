463bfc6b1f3396315a40893b85e2192fe9755e70
YERP02 <yahiremanuelramirezpulido@gmail.com>
2026-03-28 03:17:52 -0600

    refactor game logic to handle winner evaluation after dealer turn

--- game.py
+++ game.py
@@ -1,8 +1,17 @@
-
 from deck import Deck
 from hand import Hand
 
 class Game:
+
+    PLAYING = "playing"
+    BUSTED = "player_busted"
+    # GAMEOVER = "game_over"
+    DEALER_WIN = "dealer_win"
+    TIE = "tie"
+    PLAYER_WIN = "player_win"
+
+    def __init__(self):
+        self.state = Game.PLAYING
 
     def start_game(self):
         self.deck = Deck()
@@ -15,34 +24,41 @@ class Game:
             self.player_hand.add_card(self.deck.deal(1))
             self.dealer_hand.add_card(self.deck.deal(1))
 
+    #check if the player busted when take a new card
+    def player_busted(self):
+        return self.player_hand.get_value() > 21
+
+    #function for take another card
     def player_hit(self):
-        if self.player_hand.get_value() < 21:
-            self.player_hand.add_card(self.deck.deal(1))
+        self.player_hand.add_card(self.deck.deal(1))
+
+        if self.player_busted():
+            self.state = Game.DEALER_WIN
+
+    def dealer_busted(self):
+        return self.dealer_hand.get_value()> 21
 
     def dealer_turn(self):
         while self.dealer_hand.get_value() < 17:
             self.dealer_hand.add_card(self.deck.deal(1))
 
+            if self.dealer_busted():
+                self.state = Game.PLAYER_WIN
+                return
 
-    def check_winner(self, game_over= False):
-        if not game_over:
-            if self.player_hand.get_value() >21:
-                return "Gano el dealer"
-                #return True
-            elif self.dealer_hand.get_value() >21:
-                return "Ganaste"
-            elif self.dealer_hand.is_blackjack() and self.player_hand.is_blackjack():
-                return "Empate"
-            elif self.dealer_hand.is_blackjack():
-                return "Gano el dealer"
+        self.state = Game.GAMEOVER
+        self.check_winner()
+
+    def check_winner(self):
+        player = self.player_hand.get_value()
+        dealer = self.dealer_hand.get_value()
+
+        if player == dealer:
+            self.state = Game.TIE
+        elif player > dealer:
+            self.state = Game.PLAYER_WIN
         else:
-            if self.player_hand.get_value() > self.dealer_hand.get_value():
-                return "Ganaste"
-            elif self.player_hand.get_value() == self.dealer_hand.get_value():
-               return "Empate"
-            else:
-                return "Gano el dealer"
-        return "Ganaste"
+            self.state = Game.DEALER_WIN
 
     def get_player_cards(self):
         return [str(card) for card in self.player_hand.cards]
